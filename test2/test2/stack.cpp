0921d3f1d017b82dbd7e972dfeb55ac74db9dd18
Matthieu Benoit <matthieu.benoit@etu.minesparis.psl.eu>
2024-11-07 12:32:02 +0100

    add main code

---
+++ test2/test2/stack.cpp
@@ -0,0 +1,21 @@
+//
+//  stack.cpp
+//  test2
+//
+//  Created by Matthieu Benoit on 07/11/2024.
+//
+
+struct IntStack3; // création d'une pile pouvant contenir 10 éléments au maximum
+
+int stack = {0,0,0}
+
+void push(int n) {
+    for(i = 0, i < 3, i = i + 1) {
+        if(stack[i] = 0){
+            stack[i] = n;
+        }
+    }
+}
+void pop() {
+    
+}
